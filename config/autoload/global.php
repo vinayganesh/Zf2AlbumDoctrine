8f5d5d4d8e64dbdf8628388c77bb42fb7448af77
Vinay Ganesh <vganesh@soliantconsulting.com>
2013-11-20 13:57:43 -0800

    Standardize the global.php to local.php.dist

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,18 +12,4 @@
  */
 
 return array(
-    'doctrine' => array(
-        'connection' => array(
-            'orm_default' => array(
-                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
-                'params' => array(
-                    'host'     => 'localhost',
-                    'port'     => '3306',
-                    'user'     => 'root',
-                    'password' => '',
-                    'dbname'   => 'album',
-                )
-            )
-        )
-    )
 );
